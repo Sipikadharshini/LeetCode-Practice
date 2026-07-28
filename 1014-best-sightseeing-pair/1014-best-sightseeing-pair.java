131cb97dd12771d85d476f42a63954d86e016c1e
Sipikadharshini <sipikadharshini@gmail.com>
2026-07-28 21:46:45 +0530

    Time: 4 ms (90.1%), Space: 53.6 MB (97.37%) - LeetHub

---
+++ 1014-best-sightseeing-pair/1014-best-sightseeing-pair.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int maxScoreSightseeingPair(int[] values) {
+ 
+        int best = values[0];
+        int ans = 0;
+
+        for (int j = 1; j < values.length; j++) {
+
+            ans = Math.max(ans, best + values[j] - j);
+
+            best = Math.max(best, values[j] + j);
+        }
+
+        return ans;
+        
+    }
+}
